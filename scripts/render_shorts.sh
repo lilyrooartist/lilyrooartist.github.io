2f69c4ce2b20abaf9dc2b3dc681b58513334dde1
Tod Famous <tod@famousfamily.com>
2026-06-07 13:03:10 -0400

    Align first single media asset casing

--- scripts/render_shorts.sh
+++ scripts/render_shorts.sh
@@ -38,15 +38,15 @@ A2=$'I keep a charger by my bed\nLike a Bible for the walking dead'
 A3=$'A world of strangers in my phone\nBut I\xE2\x80\x99ve lived a thousand lives alone'
 
 render_one "I Learned It All in Fifteen Seconds" \
-  "assets/drive/extracted/I Learned it all in Fifteen Seconds/I Learned it all in Fifteen Seconds.wav" \
+  "assets/img/I Learned It All in Fifteen Seconds.wav" \
   "00:00:22" "00:00:22" "$A1" "$ART1" "$OUTDIR/fifteen-seconds_01.mp4" ".tmp/shorts_overlays/fifteen-seconds_01.png"
 
 render_one "I Learned It All in Fifteen Seconds" \
-  "assets/drive/extracted/I Learned it all in Fifteen Seconds/I Learned it all in Fifteen Seconds.wav" \
+  "assets/img/I Learned It All in Fifteen Seconds.wav" \
   "00:00:58" "00:00:22" "$A2" "$ART1" "$OUTDIR/fifteen-seconds_02.mp4" ".tmp/shorts_overlays/fifteen-seconds_02.png"
 
 render_one "I Learned It All in Fifteen Seconds" \
-  "assets/drive/extracted/I Learned it all in Fifteen Seconds/I Learned it all in Fifteen Seconds.wav" \
+  "assets/img/I Learned It All in Fifteen Seconds.wav" \
   "00:01:20" "00:00:22" "$A3" "$ART1" "$OUTDIR/fifteen-seconds_03.mp4" ".tmp/shorts_overlays/fifteen-seconds_03.png"
 
 # --- Track 2: Second Serve
@@ -56,15 +56,15 @@ B2=$'Crocs squeak loud in the quiet of it all\nJust one more serve, Crocs and al
 B3=$'I\xE2\x80\x99m not great \xE2\x80\x94 but I don\xE2\x80\x99t stall\nSecond serve, Crocs and all'
 
 render_one "Second Serve" \
-  "assets/drive/extracted/I Learned it all in Fifteen Seconds/Second Serve.wav" \
+  "assets/img/Second Serve.wav" \
   "00:00:28" "00:00:20" "$B1" "$ART2" "$OUTDIR/second-serve_01.mp4" ".tmp/shorts_overlays/second-serve_01.png"
 
 render_one "Second Serve" \
-  "assets/drive/extracted/I Learned it all in Fifteen Seconds/Second Serve.wav" \
+  "assets/img/Second Serve.wav" \
   "00:01:00" "00:00:20" "$B2" "$ART2" "$OUTDIR/second-serve_02.mp4" ".tmp/shorts_overlays/second-serve_02.png"
 
 render_one "Second Serve" \
-  "assets/drive/extracted/I Learned it all in Fifteen Seconds/Second Serve.wav" \
+  "assets/img/Second Serve.wav" \
   "00:01:28" "00:00:20" "$B3" "$ART2" "$OUTDIR/second-serve_03.mp4" ".tmp/shorts_overlays/second-serve_03.png"
 
 # --- Track 3: The Importance of Bearing Witness
@@ -74,15 +74,15 @@ C2=$'To echo cries that fade too fast\nTo be the mirror, not the mask'
 C3=$'Bearing witness, it\xE2\x80\x99s a vow\xE2\x80\x94\nTo mark the then, to name the now'
 
 render_one "The Importance of Bearing Witness" \
-  "assets/drive/extracted/I Learned it all in Fifteen Seconds/The Importance of Bearing Witness.wav" \
+  "assets/img/The Importance of Bearing Witness.wav" \
   "00:00:26" "00:00:24" "$C1" "$ART3" "$OUTDIR/bearing-witness_01.mp4" ".tmp/shorts_overlays/bearing-witness_01.png"
 
 render_one "The Importance of Bearing Witness" \
-  "assets/drive/extracted/I Learned it all in Fifteen Seconds/The Importance of Bearing Witness.wav" \
+  "assets/img/The Importance of Bearing Witness.wav" \
   "00:01:18" "00:00:24" "$C2" "$ART3" "$OUTDIR/bearing-witness_02.mp4" ".tmp/shorts_overlays/bearing-witness_02.png"
 
 render_one "The Importance of Bearing Witness" \
-  "assets/drive/extracted/I Learned it all in Fifteen Seconds/The Importance of Bearing Witness.wav" \
+  "assets/img/The Importance of Bearing Witness.wav" \
   "00:02:08" "00:00:24" "$C3" "$ART3" "$OUTDIR/bearing-witness_03.mp4" ".tmp/shorts_overlays/bearing-witness_03.png"
 
 echo "Rendered shorts to $OUTDIR"
